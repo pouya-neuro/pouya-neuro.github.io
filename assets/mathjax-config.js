d3b072bd0047c47df39b6e7e9d2f8fb435a54a7c
pouya-neuro <95030668+pouya-neuro@users.noreply.github.com>
2025-12-20 17:54:43 +0200

    added chapter 4 summary for theory and reality and mathjax functionality

---
+++ assets/mathjax-config.js
@@ -0,0 +1,17 @@
+// MathJax configuration shared across the site
+window.MathJax = {
+  tex: {
+    inlineMath: [
+      ['$', '$'],
+      ['\\(', '\\)']
+    ],
+    displayMath: [
+      ['$$', '$$'],
+      ['\\[', '\\]']
+    ],
+    processEscapes: true
+  },
+  options: {
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
+  }
+};
